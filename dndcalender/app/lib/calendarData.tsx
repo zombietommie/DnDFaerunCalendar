e8c8ebfb4ed6274c4fa52e00e1f6048cf22a7bde
tommy <zombietommie@gmail.com>
2025-07-11 23:47:59 -0500

    date data added

---
+++ dndcalender/app/lib/calendarData.tsx
@@ -0,0 +1,24 @@
+export const calendarData = {
+  year: 1492,
+  months: [
+    { name: "Hammer", days: 30, postition: 1 },
+    { name: "Alturiak", days: 30, postition: 2 },
+    { name: "Ches", days: 30, postition: 3 },
+    { name: "Tarsakh", days: 30, postition: 4 },
+    { name: "Mirtul", days: 30, postition: 5 },
+    { name: "Kythorn", days: 30, postition: 6 },
+    { name: "Flamerule", days: 30, postition: 7 },
+    { name: "Eleasis", days: 30, postition: 8 },
+    { name: "Elint", days: 30, postition: 9 },
+    { name: "Marpenoth", days: 30, postition: 10 },
+    { name: "Uktar", days: 30, postition: 11 },
+    { name: "Nightal", days: 30, postition: 12 },
+  ],
+  holidays: [
+    { name: "Midwinter", afterMonthPosition: 0 }, // Position 0 mean before the first month
+    { name: "Greengrass", afterMonthPosition: 3 },
+    { name: "Midsummer", afterMonthPosition: 6 },
+    { name: "Highharvestide", afterMonthPosition: 8 },
+    { name: "Feast of the Moon", afterMonthPosition: 10 },
+  ],
+};
