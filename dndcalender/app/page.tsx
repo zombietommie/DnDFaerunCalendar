aaa874675871ca613dbff66476c0bd4d7e464d0b
Tommy Tran <zombietommie@gmail.com>
2025-07-12 01:01:11 -0500

    removed some styling

--- dndcalender/app/page.tsx
+++ dndcalender/app/page.tsx
@@ -2,12 +2,12 @@ import Calendar from "./components/Calendar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center">
+      <main>
         <h1>Calendar of Harptos</h1>
         {<Calendar />}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+      <footer></footer>
     </div>
   );
 }
